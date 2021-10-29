cca8dc37247793b3cce9d5f75d68b6028143c4d4
Dmitrii-Kozlov <demetra_n@front.ru>
2021-10-29 18:48:22 +0300

    add ability to add product to cart

--- carts/views.py
+++ carts/views.py
@@ -8,12 +8,18 @@ def cart_home(request):
     return render(request, 'carts/view.html', {})
 
 def cart_update(request):
-    product_id = 1
-    cart_obj, is_new_obj = Cart.objects.get_or_create(request)
-    product_obj = Product.objects.get(id=product_id)
-    if product_obj not in cart_obj.products.all():
-        cart_obj.products.add(product_obj)
-    else:
-        cart_obj.products.remove(product_obj)
-    return redirect('carts:home')
+    # print(request.POST)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            cart_obj, is_new_obj = Cart.objects.get_or_create(request)
+        except Product.DoesNotExist:
+            print("Message for user, product gone?")
+            return redirect('carts:home')
+        product_obj = Product.objects.get(id=product_id)
+        if product_obj not in cart_obj.products.all():
+            cart_obj.products.add(product_obj)
+        else:
+            cart_obj.products.remove(product_obj)
+    return redirect('products:detail', product_obj.slug)
 
